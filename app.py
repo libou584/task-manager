44d82bcc3e17dcfd82213a46f62ea0b9887c2360
lilian <li4bou@gmail.com>
2024-02-23 15:05:05 +0100

    working app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, render_template, request
+from flask import Flask, g, render_template, request, redirect, url_for
 import sqlite3
 import datetime
 # import os
@@ -28,20 +28,39 @@ def close_connection(exception) :
 
 
 @app.route("/", methods = ['GET', 'POST'])
-def display() :
+def homepage() :
 	connection = get_db()
 	c = connection.cursor()
 	c.execute("SELECT id, title, description, tag FROM Tasks WHERE status = 'ndone' ORDER BY priority ;")
 	task_list = c.fetchall()
-	# id_list = [str(task[0]) for task in task_list]
-	# print(id_list)
-	# id = request.args.get("button")
-	# print(id)
-	# if request.method == 'POST' :
-	# 	for id in id_list :
-	# 		btn_id = request.form[id]
-	# 	print(btn_id)
+	if request.method == 'POST' :
+		if 'modify' in request.form :
+			id = request.form['modify']
+			return redirect(url_for("modify", id = id))
+		if 'done' in request.form :
+			id = request.form['done']
+			c.execute("UPDATE Tasks SET status = 'done' WHERE id = (?)", id)
+			connection.commit()
+			return redirect(url_for("homepage"))
 	return render_template("homepage.html", task_list = task_list)
+
+
+@app.route("/<int:id>/modify")
+def modify(id : int) :
+	connection = get_db()
+	c = connection.cursor()
+	c.execute('SELECT title, description, tag, priority FROM Tasks WHERE id = (?) ;', (id, ))
+	title, description, tag, priority = c.fetchall()[0]
+	if request.method == 'POST' :
+		title = request.form['title']
+		description = request.form['description']
+		tag = request.form['tag']
+		priority = request.form['priority']
+		if priority is None :
+			priority = 5
+		c.execute("UPDATE Tasks SET title = (?), description = (?), tag = (?), priority = (?) WHERE id = (?)", (title, description, tag, priority, id))
+		connection.commit()
+	return render_template("modify.html", title = title)
 
 
 @app.route("/add_task", methods = ['GET', 'POST'])
@@ -59,10 +78,24 @@ def add_task() :
 		description = request.form['description']
 		tag = request.form['tag']
 		priority = request.form['priority']
-		if not priority : 
+		if priority is None :
 			priority = 5
 		status = "ndone"
 		date = str(datetime.date.today())
-		c.execute(f"INSERT INTO Tasks(id, title, description, status, date, tag, priority) VALUES (?, ?, ?, ?, ?, ?, ?) ;", (id, title, description, status, date, tag, priority))
+		c.execute("INSERT INTO Tasks(id, title, description, status, date, tag, priority) VALUES (?, ?, ?, ?, ?, ?, ?) ;", (id, title, description, status, date, tag, priority))
 		connection.commit()
 	return render_template("add_task.html")
+
+
+@app.route("/done_tasks", methods = ['GET', 'POST'])
+def done_tasks() :
+	connection = get_db()
+	c = connection.cursor()
+	c.execute("SELECT id, title, description, tag FROM Tasks WHERE status = 'done' ORDER BY priority ;")
+	done_task_list = c.fetchall()
+	if request.method == 'POST' :
+		id = request.form['undo']
+		c.execute("UPDATE Tasks SET status = 'ndone' WHERE id = (?)", id)
+		connection.commit()
+		return redirect(url_for("homepage"))
+	return render_template("done_tasks.html", done_task_list = done_task_list)
